9f0440b9bba4bfa042aec85933d93fa77e651ebc
eldanka <eldankahrovic2208@gmail.com>
2021-03-19 10:20:03 +0100

    Adding ex10

--- ex10/src/index.js
+++ ex10/src/index.js
@@ -7,3 +7,4 @@ console.log(mySplice([1, 2, 3], [4, 5], 1));
 console.log(mySplice([1, 2, 3], [4, 5], 2));
 console.log(mySplice(["b", "c"], ["a", "d"], 1));
 console.log(mySplice(["Dell", "Philips"], ["Samsung", "AOX", "Sony"], 2));
+module.exports = mySplice;

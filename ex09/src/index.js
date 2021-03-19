9fa40205abed909b2afdf5583dbadd07619da331
eldanka <eldankahrovic2208@gmail.com>
2021-03-19 09:53:51 +0100

    Adding ex09

--- ex09/src/index.js
+++ ex09/src/index.js
@@ -6,3 +6,4 @@ console.log(myBouncer([7, "ate", "", false, 9]));
 console.log(myBouncer(["a", "b", "c"]));
 console.log(myBouncer([false, null, 0, NaN, undefined, ""]));
 console.log(myBouncer([null, NaN, 1, 2, undefined]));
+module.exports = myBouncer;
